ea601872f006a286ddc4f9f40dd4df2ccd0b5e72
sunzhou <22111520037@m.fudan.edu.cn>
2025-04-04 16:54:07 +0800

    fix @in path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: process.env.NODE_ENV === 'production' ? '/ecgps/' : '/', // 设置基础路径为仓库名
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
